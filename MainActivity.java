a2a26b20b0c371344cedd662f623cd960af01fd9
cityking <154443603@qq.com>
2016-06-13 21:58:12 +0800

    使用谷歌提供的API操作数据库

--- MainActivity.java
+++ MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.createdb;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -21,26 +23,45 @@ public class MainActivity extends Activity {
 	public void add(View v) {
 		// 获取数据库 第一次调用时创建数据库
 		SQLiteDatabase mydb = sqliteOpenHelper.getWritableDatabase();
-		mydb.execSQL("INSERT INTO info (name,phone) VALUES (?,?)", new Object[] { "cityking", "18221339272" });
+		ContentValues values = new ContentValues();
+		values.put("name", "chenhai");
+		values.put("phone", "13166185977");
+		long insert = mydb.insert("info", null, values );
+		if(insert>0){
+		Toast.makeText(getApplicationContext(), "添加成功", 0).show();
+		}else{
+			Toast.makeText(getApplicationContext(), "添加失败", 0).show();
+		}
+//		mydb.execSQL("INSERT INTO info (name,phone) VALUES (?,?)", new Object[] { "cityking", "18221339272" });
+		mydb.close();
 	}
 
 	public void delete(View v) {
 		SQLiteDatabase mydb = sqliteOpenHelper.getWritableDatabase();
-		mydb.execSQL("DELETE FROM info WHERE name =?", new Object[] { "cityking" });
+		int delete = mydb.delete("info", "name=?", new String[]{"chenhai"});
+//		mydb.execSQL("DELETE FROM info WHERE name =?", new Object[] { "cityking" });
+		Toast.makeText(getApplicationContext(), "删除了"+delete+"行", 0).show();
+		mydb.close();
 	}
 
 	public void update(View v) {
 		SQLiteDatabase mydb = sqliteOpenHelper.getWritableDatabase();
-		mydb.execSQL("UPDATE info SET name=? WHERE name =?", new Object[] { "王城", "cityking" });
+//		mydb.execSQL("UPDATE info SET name=? WHERE name =?", new Object[] { "王城", "cityking" });
+		ContentValues values = new ContentValues();
+		values.put("phone", "13882547894");
+		int update = mydb.update("info", values, "name=?", new String[]{"chenhai"});
+		Toast.makeText(getApplicationContext(), "更新了"+update+"行", 0).show();
+		mydb.close();
 	}
 
 	public void find(View v) {
 		SQLiteDatabase mydb = sqliteOpenHelper.getReadableDatabase();
-		Cursor cursor = mydb.rawQuery("select * from info", null);
+//		Cursor cursor = mydb.rawQuery("select name,phone from info", null);
+		Cursor cursor = mydb.query("info", new String[]{"name","phone"}, null, null, null, null, null);
 		if (cursor != null && cursor.getCount() > 0) {
 			while (cursor.moveToNext()) {
-				String name = cursor.getString(1);
-				String phone = cursor.getString(2);
+				String name = cursor.getString(0);
+				String phone = cursor.getString(1);
 				System.out.println("name=" + name + ",phone=" + phone);
 			}
 		}
